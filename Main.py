9d5a80e81da3970e91d3dd65868d8b4412fcb417
Thunder Bolt <bolt.thunderbolt168@gmail.com>
2024-07-01 22:23:19 +0700

    Menyusun urutan eksekusi program, menambahkan validasi input operator, menambah karakter operator disebelah nama operatornya menggunakan multi dimensional array

--- Main.py
+++ Main.py
@@ -1,23 +1,10 @@
 
 ## Creating a simple calculator
 
-arithmeticOperator = ['Addition', 'Subtraction', 'Multiplication', 'Division'];
-
 def showArithmeticOperator() :
   for index, operator in enumerate(arithmeticOperator, start=1) :
     print(f"{index}. {operator}");
 
-print("Simple calculator with Python");
-print("=============================\n")
-
-firstNumber = float(input('Masukkan angka pertama:\n-> '));
-showArithmeticOperator();
-chooseTheArithmeticOperator = int(input('\nPilih operator sesuai yang ada diatas:\nNote: Pilih operator sesuai nomor!\n\n-> '));
-chooseTheArithmeticOperator = chooseTheArithmeticOperator - 1; # rumus menyesuaikan urutan index array arithmeticOperator
-print(f'\nkamu memilih: {arithmeticOperator[chooseTheArithmeticOperator]}'); # Menampilkan rumus yang dipilih
-secondNumber = float(input('\nMasukkan angka kedua:\n-> '));
-
-# Function
 def calculation(firstNumber, chooseTheArithmeticOperator, secondNumber) :
   if chooseTheArithmeticOperator == arithmeticOperator[0] :
     return firstNumber + secondNumber;
@@ -28,5 +15,38 @@ def calculation(firstNumber, chooseTheArithmeticOperator, secondNumber) :
   elif chooseTheArithmeticOperator == arithmeticOperator[3] :
     return firstNumber / secondNumber;
 
+def validateInputOperator() :
+  n = len(arithmeticOperator);
+  index = chooseTheArithmeticOperator;
+  if 0 <= index <n :
+    # Jika nilai index valid, maka kembalikan nilai
+    return;
+  else :
+    # print(f'Masukkan pilihan sesuai nomor yang tertera dari 1 - {n}. Coba lagi!');
+    raise IndexError(f'Masukkan pilihan sesuai nomor yang tertera dari 1 - {n}. Coba lagi!');
+
+
+print("Simple calculator with Python");
+print("=============================\n")
+
+arithmeticOperator = [
+  ['Addition', '+'],
+  ['Subtraction','-'],
+  ['Multiplication','*'],
+  ['Division','/']
+  ];
+
+# Input dari User
+firstNumber = float(input('Masukkan angka pertama:\n-> '));
+
+showArithmeticOperator();
+chooseTheArithmeticOperator = int(input('\nPilih operator sesuai yang ada diatas:\nNote: Pilih operator sesuai nomor!\n\n-> '));
+validateInputOperator();
+chooseTheArithmeticOperator = chooseTheArithmeticOperator - 1; # rumus menyesuaikan urutan index array arithmeticOperator
+print(f'\nKamu memilih: {arithmeticOperator[chooseTheArithmeticOperator]}'); # Menampilkan rumus yang dipilih
+
+secondNumber = float(input('\nMasukkan angka kedua:\n-> '));
+
+# Tampilkan Hasil
 result = calculation(firstNumber, arithmeticOperator[chooseTheArithmeticOperator], secondNumber);
-print(f"\nHasilnya dari: {result}");
+print(f"\nHasil dari {firstNumber} {arithmeticOperator[chooseTheArithmeticOperator][1]} {secondNumber} adalah {result}");
