e5f1230d2eb3992b6b18507cc88075fe277588a0
Thunder Bolt <bolt.thunderbolt168@gmail.com>
2024-08-07 19:18:32 +0700

    Validasi input user memilih operator sudah di fix. Tujuannya jika user memasukkan nilai 0 dan diluar list operator maka akan user akan disuruh memasukkan kembali nilainya dengan benar

--- Main.py
+++ Main.py
@@ -1,62 +1,35 @@
-
+import calculator
 ## Creating a simple calculator
 
-def showArithmeticOperator() :
-  for index, operator in enumerate(arithmeticOperator, start=1) :
-    print(f"{index}. {operator[0]}");
+def main():
+  user_input_number()
+  operator_selected()
+  showResults()
 
-def calculation(firstNumber, chooseTheArithmeticOperator, secondNumber) :
-  if chooseTheArithmeticOperator == arithmeticOperator[0] :
-    return firstNumber + secondNumber;
-  elif chooseTheArithmeticOperator == arithmeticOperator[1] :
-    return firstNumber - secondNumber;
-  elif chooseTheArithmeticOperator == arithmeticOperator[2] :
-    return firstNumber * secondNumber;
-  elif chooseTheArithmeticOperator == arithmeticOperator[3] :
-    return firstNumber / secondNumber;
-  elif chooseTheArithmeticOperator == arithmeticOperator[4] :
-    return firstNumber % secondNumber;
-  elif chooseTheArithmeticOperator == arithmeticOperator[5] :
-    return firstNumber ** secondNumber;
-  elif chooseTheArithmeticOperator == arithmeticOperator[6] :
-    return firstNumber // secondNumber;
+def user_input_number():
+  global firstNumber, secondNumber
+  firstNumber = float(input('Masukkan angka pertama:\n-> '));
+  secondNumber = float(input('\nMasukkan angka kedua:\n-> '));
 
-def validateInputOperator() :
-  n = len(arithmeticOperator);
-  index = chooseTheArithmeticOperator;
-  if 0 <= index <= n :
-    # Jika nilai index valid, maka kembalikan nilai
-    return;
-  else :
-    # print(f'Masukkan pilihan sesuai nomor yang tertera dari 1 - {n}. Coba lagi!');
-    raise IndexError(f'Masukkan pilihan sesuai nomor yang tertera dari 1 - {n}. Coba lagi!');
+def operator_selected():
+  global chooseTheArithmeticOperator
+  while True:
+    calculator.showArithmeticOperator();
+    chooseTheArithmeticOperator = int(input('\nPilih operator sesuai yang ada diatas:\nNote: Pilih operator sesuai nomor!\n\n-> '));
 
+    n = len(calculator.arithmeticOperator); # panjang data array (bukan urutan index)
+    if chooseTheArithmeticOperator < 1 or chooseTheArithmeticOperator > n :
+      # Jika nilai chooseTheArithmeticOperator tidak valid, maka kembalikan nilai
+      print(f'Masukkan pilihan sesuai nomor yang tertera dari nomor 1- {n}. Coba lagi!\n');
+    elif chooseTheArithmeticOperator >= 0 or chooseTheArithmeticOperator < n:
+      chooseTheArithmeticOperator = chooseTheArithmeticOperator - 1; # rumus menyesuaikan urutan index array arithmeticOperator
+      print(f'\nKamu memilih: {calculator.arithmeticOperator[chooseTheArithmeticOperator][0]}'); # Menampilkan rumus yang dipilih
+      break;
 
-print("Simple calculator with Python");
-print("=============================\n")
+def showResults():
+  # Tampilkan Hasil
+  result = calculator.calculation(firstNumber, calculator.arithmeticOperator[chooseTheArithmeticOperator], secondNumber);
+  print(f"Hasil dari {firstNumber} {calculator.arithmeticOperator[chooseTheArithmeticOperator][1]} {secondNumber} adalah {result}");
 
-arithmeticOperator = [
-  ['Addition', '+'],
-  ['Subtraction','-'],
-  ['Multiplication','*'],
-  ['Division','/'],
-  ['Modulus','%'],
-  ['Exponentiation','**'],
-  ['Floor division','//']
-  ];
-
-# Input dari User
-firstNumber = float(input('Masukkan angka pertama:\n-> '));
-print(end="\n");
-
-showArithmeticOperator();
-chooseTheArithmeticOperator = int(input('\nPilih operator sesuai yang ada diatas:\nNote: Pilih operator sesuai nomor!\n\n-> '));
-validateInputOperator();
-chooseTheArithmeticOperator = chooseTheArithmeticOperator - 1; # rumus menyesuaikan urutan index array arithmeticOperator
-print(f'\nKamu memilih: {arithmeticOperator[chooseTheArithmeticOperator][0]}'); # Menampilkan rumus yang dipilih
-
-secondNumber = float(input('\nMasukkan angka kedua:\n-> '));
-
-# Tampilkan Hasil
-result = calculation(firstNumber, arithmeticOperator[chooseTheArithmeticOperator], secondNumber);
-print(f"\nHasil dari {firstNumber} {arithmeticOperator[chooseTheArithmeticOperator][1]} {secondNumber} adalah {result}");
+if __name__ == '__main__':
+  main()
--- README.md
+++ README.md
@@ -9,3 +9,8 @@ Creating an simple calculator with python
 - **Modulus** (%)
 - **Exponentiation** (**)
 - **Floor division** (//)
+
+## Update
+### 📅 7/8/2024
+- Menggunakan function dan library untuk memisahkan program utama dan library calculator
+- Validasi input user memilih operator sudah di fix. Tujuannya jika user memasukkan nilai 0 dan diluar list operator maka akan user akan disuruh memasukkan kembali nilainya dengan benar
